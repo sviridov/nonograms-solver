78a6f74f58b56aec131ab5abbf6a0dfc90e5e591
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-07-27 16:33:12 +0600

    Fixed typo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject nanograms-solver "0.1.0-SNAPSHOT"
-  :description "Nanograms solver powered by clojure.core.logic"
-  :url "https://github.com/sviridov/nanograms-solver"
+(defproject nonograms-solver "0.1.0-SNAPSHOT"
+  :description "Nonograms solver powered by clojure.core.logic"
+  :url "https://github.com/sviridov/nonograms-solver"
   :license {:name "The MIT License"
             :url  "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
