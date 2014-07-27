390be9ab3a88140dbe72cf4772c4f4c1d1dda575
Sviridov Alexander <sviridov.vmi@gmail.com>
2014-07-27 15:55:47 +0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject nanograms-solver "0.1.0-SNAPSHOT"
+  :description "Nanograms solver powered by clojure.core.logic"
+  :url "https://github.com/sviridov/nanograms-solver"
+  :license {:name "The MIT License"
+            :url  "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.8"]])
